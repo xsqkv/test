ac05ec0178cf832009144a506276a047eecf426f
Artem Vanty <artem.vanty.0358@gmail.com>
2021-12-04 00:51:36 +0900

    main

--- test.cpp
+++ test.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
 
 using namespace std;
 
-int main()
+void setcur(int x,int y)
 {
 
+}
+void getcur(int *x,int *y)
+{
+
+}
+
+int main()
+{
+	struct winsize buff;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &buff);
+	int w = buff.ws_col;
+	int h = buff.ws_row;
+	
+	return 0;
 }
